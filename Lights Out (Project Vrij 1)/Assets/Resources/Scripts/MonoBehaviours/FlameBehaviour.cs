d1ae6b5c30cfa3b8e510f501a9c5f67b5d69830e
N. Weber <ketoleperouch@gmail.com>
2018-05-24 14:56:09 +0200

    Prototype V0.0.0.0000.01
    Flame Behaviour in Progress!

---
+++ Lights Out (Project Vrij 1)/Assets/Resources/Scripts/MonoBehaviours/FlameBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+
+public class FlameBehaviour : MonoBehaviour {
+
+    [Range(0, 100)]
+    public float vitality = 100f;
+    public float diminishMultiplier = 5f;
+    public float pulseRange = 5f;
+
+    public GameObject pulse;
+    public AudioClip noPulseClip; 
+
+    private ParticleSystem particles;
+    private float originalROT;
+    private float originalLightIntensity;
+    private Light flameLight;
+
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+        flameLight = GetComponentInChildren<Light>(false);
+        originalROT = particles.emission.rateOverTime.constant;
+        originalLightIntensity = flameLight.intensity;
+    }
+
+    private void Update()
+    {
+        vitality -= Time.deltaTime * diminishMultiplier;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Pulse();
+        }
+
+        SetValues();
+    }
+
+    private void SetValues()
+    {
+        flameLight.intensity = originalLightIntensity * (vitality / 100);
+        SetEmission();
+        SetStartColor();
+    }
+
+    private void SetStartColor()
+    {
+        var startColor = particles.main;
+        startColor.startColor = new Color(startColor.startColor.color.r, startColor.startColor.color.g, startColor.startColor.color.b, vitality / 100);
+    }
+
+    private void SetEmission()
+    {
+        ParticleSystem.MinMaxCurve newRate = new ParticleSystem.MinMaxCurve();
+        newRate.constant = originalROT * (vitality / 100);
+
+        var emission = particles.emission;
+        emission.rateOverTime = newRate.constant;
+    }
+
+    private void Pulse()
+    {
+        //Pulse can only be activated when the vitality is sufficient. Otherwise, play a "cannot do this" sound.
+        if (vitality > 10f)
+        {
+            Instantiate(pulse, transform);
+            //Search for interactables and activate them:
+            Collider[] pulseHits = Physics.OverlapSphere(transform.position, pulseRange);
+            for (int i = 0; i < pulseHits.Length; i++)
+            {
+                InteractableBase interactable = pulseHits[i].GetComponent<InteractableBase>();
+
+                if (interactable)
+                {
+                    interactable.Activate();
+                }
+            }
+        }
+        else
+        {
+            AudioSource.PlayClipAtPoint(noPulseClip, transform.position);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, pulseRange);
+    }
+}
---
+++ Lights Out (Project Vrij 1)/Assets/Resources/Scripts/MonoBehaviours/PlaySound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySound : MonoBehaviour {
+
+	AudioSource source;
+	void Start()
+	{
+        source = GetComponent<AudioSource> ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+        if (!source.isPlaying)
+        {
+            if (other.CompareTag("Player"))
+            {
+                source.Play();
+                source.Play(44100);
+            }
+        }
+	}
+}
