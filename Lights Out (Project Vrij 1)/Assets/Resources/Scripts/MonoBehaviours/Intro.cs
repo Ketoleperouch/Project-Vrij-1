c55b6e20904671a89596d7aa129c615503389362
DatBoiiiii334 <chrisaconquet@hotmail.com>
2018-06-15 14:37:18 +0200

    Add files via upload

---
+++ Lights Out (Project Vrij 1)/Assets/Resources/Scripts/MonoBehaviours/Intro.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour {
+
+    public float time = 10;
+    public float timeV2 = 1;
+
+    IEnumerator Start()
+    {
+        StartCoroutine(FadeTextToFullAlpha(1f, GetComponent<Text>()));
+        yield return new WaitForSeconds(time);
+        StartCoroutine(FadeTextToZeroAlpha(1f, GetComponent<Text>()));
+        yield return new WaitForSeconds(timeV2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+  
+
+    public IEnumerator FadeTextToFullAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+        while (i.color.a < 1.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeTextToZeroAlpha(float t, Text i)
+    {
+        i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+        while (i.color.a > 0.0f)
+        {
+            i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+            yield return null;
+        }
+    }
+
+}
